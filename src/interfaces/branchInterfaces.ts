3240a01a32b52674843d22172d20ba50d20eaa45
gutherduarte <cgdr010601@gmail.com>
2024-10-18 13:14:22 -0600

    Update branch interfaces

---
+++ src/interfaces/branchInterfaces.ts
@@ -0,0 +1,22 @@
+export interface IBranchSlice {
+  status: IStatus;
+  error: string | null;
+  branches: IBranch[];
+}
+
+export interface IBranch {
+  id: string;
+  pais: string;
+  ciudad: string;
+  nombre: string;
+  telefono: string;
+  direccion: string;
+  pendingOrders: number;
+  hasAccess: boolean;
+}
+
+export interface IBranchProps {
+  branch: IBranch;
+}
+
+export type IStatus = 'idle' | 'loading' | 'succeeded' | 'failed';
